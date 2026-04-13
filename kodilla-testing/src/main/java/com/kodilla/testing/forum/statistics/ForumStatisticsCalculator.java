6b516d29a6002270fd7699f4585595e1f67747e6
SlawomirDeja <u0540579977@gmail.com>
2026-04-13 18:06:21 +0200

    6.7 Added task 6.7

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatisticsCalculator.java
@@ -0,0 +1,58 @@
+package com.kodilla.testing.forum.statistics;
+
+public class ForumStatisticsCalculator {
+
+    private int numberOfUsers = 0;
+    private int numberOfPosts = 0;
+    private int numberOfComments = 0;
+    private float averageNumberOfPostsPerUser = 0;
+    private float averageNumberOfCommentsPerUser = 0;
+    private float averageNumberOfCommentsPerPost = 0;
+
+    private boolean numberOfUsersDivisionByZero;
+    private boolean numberOfPostsDivisionByZero;
+
+    public float getAverageNumberOfPostsPerUser() {
+        return averageNumberOfPostsPerUser;
+    }
+
+    public float getAverageNumberOfCommentsPerUser() {
+        return averageNumberOfCommentsPerUser;
+    }
+
+    public float getAverageNumberOfCommentsPerPost() {
+        return averageNumberOfCommentsPerPost;
+    }
+
+    public boolean isNumberOfUsersDivisionByZero() {
+        return numberOfUsersDivisionByZero;
+    }
+
+    public boolean isNumberOfPostsDivisionByZero() {
+        return numberOfPostsDivisionByZero;
+    }
+
+    public void calculateAdvStatistics(Statistics statistics) {
+
+        numberOfUsers = statistics.usersNames().size();
+        numberOfPosts = statistics.postsCount();
+        numberOfComments = statistics.commentsCount();
+
+        if (numberOfUsers == 0)
+            numberOfUsersDivisionByZero = true;
+            else {
+                numberOfUsersDivisionByZero = false;
+                averageNumberOfPostsPerUser = (float) numberOfPosts / numberOfUsers;
+                averageNumberOfCommentsPerUser = (float) numberOfComments / numberOfUsers;
+            }
+
+        if (numberOfPosts == 0)
+            numberOfPostsDivisionByZero = true;
+            else {
+                numberOfPostsDivisionByZero = false;
+                averageNumberOfCommentsPerPost = (float) numberOfComments / numberOfPosts;
+            }
+
+    }
+
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsCalculatorTestSuite.java
@@ -0,0 +1,142 @@
+package com.kodilla.testing.forum.statistics;
+
+import com.kodilla.testing.library.Book;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ForumStatisticsCalculatorTestSuite {
+
+    private List<String> generateListOfNUserNames(int userNamesQuantity) {
+        List<String> resultList = new ArrayList<>();
+        for (int n = 1; n <= userNamesQuantity; n++)
+            resultList.add("username" + n);
+        return resultList;
+    }
+
+    @Mock
+    private Statistics statisticsMock;
+
+    @Test
+    void testCalculateAdvStatisticsTheNumberOfPostsIs0() {
+        //Given
+        ForumStatisticsCalculator forumStatisticsCalculator = new ForumStatisticsCalculator();
+        List<String> resultUsersNames = generateListOfNUserNames(3);
+        when(statisticsMock.usersNames()).thenReturn(resultUsersNames);
+        when(statisticsMock.postsCount()).thenReturn(0);
+        when(statisticsMock.commentsCount()).thenReturn(9);
+        //When
+        forumStatisticsCalculator.calculateAdvStatistics(statisticsMock);
+        //Then
+        assertEquals(0, forumStatisticsCalculator.getAverageNumberOfPostsPerUser());
+        assertEquals(3, forumStatisticsCalculator.getAverageNumberOfCommentsPerUser());
+        assertTrue(forumStatisticsCalculator.isNumberOfPostsDivisionByZero());
+    }
+
+    @Test
+    void testCalculateAdvStatisticsTheNumberOfPostsIs1000() {
+        //Given
+        ForumStatisticsCalculator forumStatisticsCalculator = new ForumStatisticsCalculator();
+        List<String> resultUsersNames = generateListOfNUserNames(4);
+        when(statisticsMock.usersNames()).thenReturn(resultUsersNames);
+        when(statisticsMock.postsCount()).thenReturn(1000);
+        when(statisticsMock.commentsCount()).thenReturn(40);
+        //When
+        forumStatisticsCalculator.calculateAdvStatistics(statisticsMock);
+        //Then
+        assertEquals(250, forumStatisticsCalculator.getAverageNumberOfPostsPerUser());
+        assertEquals(10, forumStatisticsCalculator.getAverageNumberOfCommentsPerUser());
+        assertEquals((float) 40 / 1000, forumStatisticsCalculator.getAverageNumberOfCommentsPerPost());
+    }
+
+    @Test
+    void testCalculateAdvStatisticsTheNumberOfCommentsIs0() {
+        //Given
+        ForumStatisticsCalculator forumStatisticsCalculator = new ForumStatisticsCalculator();
+        List<String> resultUsersNames = generateListOfNUserNames(4);
+        when(statisticsMock.usersNames()).thenReturn(resultUsersNames);
+        when(statisticsMock.postsCount()).thenReturn(1000);
+        when(statisticsMock.commentsCount()).thenReturn(0);
+        //When
+        forumStatisticsCalculator.calculateAdvStatistics(statisticsMock);
+        //Then
+        assertEquals(250, forumStatisticsCalculator.getAverageNumberOfPostsPerUser());
+        assertEquals(0, forumStatisticsCalculator.getAverageNumberOfCommentsPerUser());
+        assertEquals(0, forumStatisticsCalculator.getAverageNumberOfCommentsPerPost());
+    }
+
+    @Test
+    void testCalculateAdvStatisticsTheNumberOfCommentsIsLessThanTheNumberOfPosts() {
+        //Given
+        ForumStatisticsCalculator forumStatisticsCalculator = new ForumStatisticsCalculator();
+        List<String> resultUsersNames = generateListOfNUserNames(4);
+        when(statisticsMock.usersNames()).thenReturn(resultUsersNames);
+        when(statisticsMock.postsCount()).thenReturn(100);
+        when(statisticsMock.commentsCount()).thenReturn(50);
+        //When
+        forumStatisticsCalculator.calculateAdvStatistics(statisticsMock);
+        //Then
+        assertEquals(25, forumStatisticsCalculator.getAverageNumberOfPostsPerUser());
+        assertEquals((float) 50 / 4, forumStatisticsCalculator.getAverageNumberOfCommentsPerUser());
+        assertEquals((float) 50 / 100, forumStatisticsCalculator.getAverageNumberOfCommentsPerPost());
+    }
+
+    @Test
+    void testCalculateAdvStatisticsTheNumberOfCommentsIsGreaterThanTheNumberOfPosts() {
+        //Given
+        ForumStatisticsCalculator forumStatisticsCalculator = new ForumStatisticsCalculator();
+        List<String> resultUsersNames = generateListOfNUserNames(4);
+        when(statisticsMock.usersNames()).thenReturn(resultUsersNames);
+        when(statisticsMock.postsCount()).thenReturn(50);
+        when(statisticsMock.commentsCount()).thenReturn(100);
+        //When
+        forumStatisticsCalculator.calculateAdvStatistics(statisticsMock);
+        //Then
+        assertEquals((float) 50 / 4, forumStatisticsCalculator.getAverageNumberOfPostsPerUser());
+        assertEquals(25, forumStatisticsCalculator.getAverageNumberOfCommentsPerUser());
+        assertEquals(2, forumStatisticsCalculator.getAverageNumberOfCommentsPerPost());
+    }
+
+    @Test
+    void testCalculateAdvStatisticsTheNumberOfUsersIs0() {
+        //Given
+        ForumStatisticsCalculator forumStatisticsCalculator = new ForumStatisticsCalculator();
+        List<String> resultUsersNames = new ArrayList<>();
+        when(statisticsMock.usersNames()).thenReturn(resultUsersNames);
+        when(statisticsMock.postsCount()).thenReturn(10);
+        when(statisticsMock.commentsCount()).thenReturn(100);
+        //When
+        forumStatisticsCalculator.calculateAdvStatistics(statisticsMock);
+        //Then
+        assertTrue(forumStatisticsCalculator.isNumberOfUsersDivisionByZero());
+        assertEquals(10, forumStatisticsCalculator.getAverageNumberOfCommentsPerPost());
+    }
+
+    @Test
+    void testCalculateAdvStatisticsTheNumberOfUsersIs100() {
+        //Given
+        ForumStatisticsCalculator forumStatisticsCalculator = new ForumStatisticsCalculator();
+        List<String> resultUsersNames = generateListOfNUserNames(100);
+        when(statisticsMock.usersNames()).thenReturn(resultUsersNames);
+        when(statisticsMock.postsCount()).thenReturn(60);
+        when(statisticsMock.commentsCount()).thenReturn(150);
+        //When
+        forumStatisticsCalculator.calculateAdvStatistics(statisticsMock);
+        //Then
+        assertEquals((float) 60 / 100, forumStatisticsCalculator.getAverageNumberOfPostsPerUser());
+        assertEquals((float) 150 / 100, forumStatisticsCalculator.getAverageNumberOfCommentsPerUser());
+        assertEquals((float) 150 / 60, forumStatisticsCalculator.getAverageNumberOfCommentsPerPost());
+    }
+
+}
