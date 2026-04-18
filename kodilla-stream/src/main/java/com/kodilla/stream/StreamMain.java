ba40277b7c030a8d834095d795fbab5f2dcf1de3
SlawomirDeja <u0540579977@gmail.com>
2026-04-18 19:27:09 +0200

    7.2 Added task 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,8 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
@@ -44,5 +46,7 @@ public class StreamMain {
             return resultString;
                                                                                                 });
 
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
