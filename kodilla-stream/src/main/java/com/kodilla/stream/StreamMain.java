20364880dcf36341d023a0cd7bafbfcc6db23ef9
SlawomirDeja <u0540579977@gmail.com>
2026-04-18 17:03:21 +0200

    7.1 Added task 7.1

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,48 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("Beautiful string", string -> "ABC" + string);
+        poemBeautifier.beautify("Google Chrome", string -> string + "ABC");
+        poemBeautifier.beautify("Microsoft Store", string -> string.toUpperCase());
+        poemBeautifier.beautify("File Explorer", string -> string.repeat(3));
+        poemBeautifier.beautify("Windows Media Player", string -> {
+
+            String resultString = "1 ";
+
+            for (int i = 0; i < 5; i++)
+                 resultString += string + " " + string.charAt(i) + " ";
+
+            return resultString;
+                                                                                                });
+
+        poemBeautifier.beautify("Apple iPhone", string -> {
+
+            String resultString = "2 ";
+
+            resultString += string.replace('l', '1');
+            resultString += " " + resultString.replace('e', '3');
+            return resultString;
+                                                                                                });
+
+    }
+}
