5ac6f70f1ada4e3cb7be233a872c2c7057382a57
SlawomirDeja <u0540579977@gmail.com>
2026-04-23 17:10:42 +0200

    7.5 Task 7.5 updated

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,5 +1,6 @@
 package com.kodilla.stream.array;
 
+import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
@@ -7,6 +8,15 @@ public interface ArrayOperations {
     static double getAverage(int[] numbers) {
         IntStream.range(0, numbers.length).map(n -> numbers[n]).forEach(System.out::println);
 
-        return IntStream.range(0, numbers.length).map(n -> numbers[n]).average().getAsDouble();
+        OptionalDouble average = IntStream.range(0, numbers.length).map(n -> numbers[n]).average();
+
+        if (average.isPresent()) {
+
+            return average.getAsDouble();
+        }
+        else {
+
+            return 0;
+        }
     }
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -9,12 +9,27 @@ class ArrayOperationsTestSuite {
     @Test
     void testGetAverage() {
         //Given
-        int numbers[] = new int[5];
+        int numbers[] = new int[20];
         numbers[0] = 4;
         numbers[1] = 6;
         numbers[2] = 5;
         numbers[3] = 7;
         numbers[4] = 3;
+        numbers[5] = 5;
+        numbers[6] = 5;
+        numbers[7] = 5;
+        numbers[8] = 5;
+        numbers[9] = 5;
+        numbers[10] = 5;
+        numbers[11] = 5;
+        numbers[12] = 5;
+        numbers[13] = 5;
+        numbers[14] = 5;
+        numbers[15] = 5;
+        numbers[16] = 5;
+        numbers[17] = 5;
+        numbers[18] = 5;
+        numbers[19] = 5;
         //When
         //Then
         assertEquals(5, ArrayOperations.getAverage(numbers));
