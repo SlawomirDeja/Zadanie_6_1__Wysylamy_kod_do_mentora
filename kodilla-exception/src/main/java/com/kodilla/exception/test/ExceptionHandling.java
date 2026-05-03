c746f2bbc2e431ba425e32e5d51e81ef6cf17242
SlawomirDeja <u0540579977@gmail.com>
2026-05-03 18:02:10 +0200

    8.3 Task 8.3 updated

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -4,13 +4,16 @@ public class ExceptionHandling {
 
     public void handle(double x, double y) {
         SecondChallenge secondChallenge = new SecondChallenge();
+        String resultProbablyIWillThrowException = "";
 
         try {
-            secondChallenge.probablyIWillThrowException(x, y);
+            resultProbablyIWillThrowException = secondChallenge.probablyIWillThrowException(x, y);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         } finally {
-            System.out.println("I am gonna be here... always!");
+            if (!resultProbablyIWillThrowException.isEmpty()) {
+                System.out.println(resultProbablyIWillThrowException);
+            }
         }
     }
 }
--- kodilla-exception/src/test/java/com/kodilla/exception/test/ExceptionHandlingTestSuite.java
+++ kodilla-exception/src/test/java/com/kodilla/exception/test/ExceptionHandlingTestSuite.java
@@ -1,24 +1,65 @@
 package com.kodilla.exception.test;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ExceptionHandlingTestSuite {
 
+    private static SecondChallenge secondChallenge;
+
+    @BeforeAll
+    static void constructSecondChallenge() {
+        secondChallenge = new SecondChallenge();
+    }
+
     @Test
-    void testProbablyIWillThrowException() {
+    void testNo1ProbablyIWillThrowException() {
         //Given
-        SecondChallenge secondChallenge = new SecondChallenge();
         //When
         //Then
-        assertAll(
-            () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(0, 2)),
-            () -> assertDoesNotThrow(() -> secondChallenge.probablyIWillThrowException(1, 2)),
-            () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(2, 2)),
-            () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(3, 2)),
-            () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(1, 1.5))
-        );
-
+        assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(0, 2));
     }
+
+    @Test
+    void testNo2ProbablyIWillThrowException() {
+        //Given
+        //When
+        //Then
+        assertDoesNotThrow(() -> secondChallenge.probablyIWillThrowException(1, 2));
+    }
+
+    @Test
+    void testNo3ProbablyIWillThrowException() {
+        //Given
+        //When
+        //Then
+        assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(2, 2));
+    }
+
+    @Test
+    void testNo4ProbablyIWillThrowException() {
+        //Given
+        //When
+        //Then
+        assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(3, 2));
+    }
+
+    @Test
+    void testNo5ProbablyIWillThrowException() {
+        //Given
+        //When
+        //Then
+        assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(4, 2));
+    }
+
+    @Test
+    void testNo6ProbablyIWillThrowException() {
+        //Given
+        //When
+        //Then
+        assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(1, 1.5));
+    }
+
 }
