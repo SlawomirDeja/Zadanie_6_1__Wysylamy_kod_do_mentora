305a81a3e24d5c3e6e85f02ee194dd05f1ed101e
SlawomirDeja <u0540579977@gmail.com>
2026-06-03 18:03:23 +0200

    13.3 Added task 13.3

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -0,0 +1,34 @@
+package com.kodilla.spring.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BoardTestSuite {
+
+    @Test
+    void testTaskAdd() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+
+        Board board = context.getBean(Board.class);
+
+        String task3 = "task3";
+        String task9 = "task9";
+        String task7 = "task7";
+
+        board.getToDoList().tasks.add(task3);
+        board.getInProgressList().tasks.add(task9);
+        board.getDoneList().tasks.add(task7);
+
+        //When & Then
+        assertEquals(task3, board.getToDoList().tasks.get(0));
+        assertEquals(task9, board.getInProgressList().tasks.get(0));
+        assertEquals(task7, board.getDoneList().tasks.get(0));
+
+        ((ConfigurableApplicationContext) context).close();
+    }
+}
