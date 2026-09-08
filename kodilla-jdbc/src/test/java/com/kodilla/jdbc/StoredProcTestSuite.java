542ca4bc3ed1c275ee891b415503bf5aa446ea87
SlawomirDeja <u0540579977@gmail.com>
2026-09-08 20:34:54 +0200

    28.3 Task 28.3 updated

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -40,15 +40,64 @@ public class StoredProcTestSuite {
 
         // Given
         DbManager dbManager = DbManager.getInstance();
-        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER = 0";
         Statement statement = dbManager.getConnection().createStatement();
-        statement.executeUpdate(sqlUpdate);
-        String sqlCheckTable1 = "SELECT COUNT(*) AS HOW_MANY_FALSE FROM BOOKS WHERE BESTSELLER = 0";
-        String sqlCheckTable2 = "SELECT COUNT(*) AS HOW_MANY_TRUE FROM BOOKS WHERE BESTSELLER = 1";
+        statement.addBatch("""
+            CREATE TABLE BOOKS2 (
+                BOOK_ID int(11) NOT NULL AUTO_INCREMENT,
+                TITLE varchar(255) NOT NULL,
+                PUBYEAR int(4) NOT NULL,
+                BESTSELLER boolean,
+                PRIMARY KEY (BOOK_ID)
+            );
+        """);
+
+        statement.addBatch("""
+            INSERT INTO BOOKS2 (TITLE, PUBYEAR, BESTSELLER)
+                VALUES
+                    ('title1', '1942', FALSE),
+                    ('title2', '1927', FALSE),
+                    ('title3', '1925', FALSE),
+                    ('title4', '1943', FALSE),
+                    ('title5', '1933', FALSE);
+        """);
+
+        statement.addBatch("""
+            CREATE TABLE RENTS2 (
+                RENT_ID int(11) NOT NULL AUTO_INCREMENT,
+                BOOK_ID int(11) NOT NULL,
+                READER_ID int(11) NOT NULL,
+                RENT_DATE datetime NOT NULL,
+                RETURN_DATE datetime,
+                PRIMARY KEY (RENT_ID),
+                FOREIGN KEY (BOOK_ID) REFERENCES BOOKS(BOOK_ID),
+                FOREIGN KEY (READER_ID) REFERENCES READERS(READER_ID)
+            );
+        """);
+
+        statement.addBatch("""
+            INSERT INTO RENTS2 (BOOK_ID, READER_ID, RENT_DATE, RETURN_DATE)
+                VALUES
+                    (1, 1, '2026-08-17 00:00:00', null),
+                    (2, 1, '2026-08-17 00:00:00', null),
+                    (4, 1, '2026-08-17 00:00:00', '2026-08-22 00:00:00'),
+                    (1, 3, '2026-08-19 00:00:00', null),
+                    (5, 3, '2026-08-23 00:00:00', '2026-08-25 00:00:00'),
+                    (5, 4, '2026-08-17 00:00:00', '2026-08-19 00:00:00'),
+                    (5, 5, '2026-08-19 00:00:00', null),
+                    (1, 3, '2026-08-26 00:00:00', null),
+                    (2, 3, '2026-08-29 00:00:00', null),
+                    (3, 1, '2026-08-26 00:00:00', null),
+                    (2, 4, '2026-08-28 00:00:00', '2026-08-31 00:00:00');
+        """);
+
+        statement.executeBatch();
+
+        String sqlCheckTable1 = "SELECT COUNT(*) AS HOW_MANY_FALSE FROM BOOKS2 WHERE BESTSELLER = 0";
+        String sqlCheckTable2 = "SELECT COUNT(*) AS HOW_MANY_TRUE FROM BOOKS2 WHERE BESTSELLER = 1";
 
         //When
         Statement statement2 = dbManager.getConnection().createStatement();
-        String sqlProcedureCall = "CALL UpdateBestsellers()";
+        String sqlProcedureCall = "CALL UpdateBestsellers2()";
         statement.execute(sqlProcedureCall);
         ResultSet rs = statement.executeQuery(sqlCheckTable1);
         ResultSet rs2 = statement2.executeQuery(sqlCheckTable2);
@@ -62,11 +111,16 @@ public class StoredProcTestSuite {
         if (rs2.next()) {
             howManyTrue = rs2.getInt("HOW_MANY_TRUE");
         }
-        assertEquals(4, howManyFalse);
-        assertEquals(1, howManyTrue);
+        assertEquals(2, howManyFalse);
+        assertEquals(3, howManyTrue);
 
         rs.close();
         rs2.close();
+
+        statement.addBatch("DROP TABLE BOOKS2;");
+        statement.addBatch("DROP TABLE RENTS2;");
+        statement.executeBatch();
+
         statement.close();
         statement2.close();
     }
